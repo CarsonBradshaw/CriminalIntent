dd6e0fec9799107f03504eee393a960b994b9847
CarsonBradshaw <carsonabradshaw@aggiemail.usu.edu>
2017-05-14 21:52:21 -0600

    Finished through C8: Activity Abstraction.

---
+++ app/src/main/java/io/github/carsonbradshaw/criminalinent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package io.github.carsonbradshaw.criminalinent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Carson on 5/13/2017.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
---
+++ app/src/main/java/io/github/carsonbradshaw/criminalinent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package io.github.carsonbradshaw.criminalinent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Carson on 5/13/2017.
+ */
+
+public class CrimeListFragment extends Fragment {
+    
+}
